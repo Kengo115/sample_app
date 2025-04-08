0fefe8c54f5d65ff2e7fc568b8faebd256c29821
Kengo115 <bp21007@shibaura-it.ac.jp>
2025-04-08 19:12:55 +0900

    Inplement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      # 保存されているユーザーのセッションを開始
+      reset_session
+      log_in user
+      redirect_to user
+    else
+      # 認証に失敗した場合の処理 
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url, status: :see_other
+  end
+end
