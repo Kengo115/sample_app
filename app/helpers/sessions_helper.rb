91f78ac71a133a120c1ee6962be7a63226fae14e
Kengo115 <bp21007@shibaura-it.ac.jp>
2025-04-09 15:30:58 +0900

    Inplement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,19 +3,46 @@ module SessionsHelper
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
+    #セッションリプレイ攻撃から保護する
+    session[:session_token] = user.session_token
+  end
+
+  # 永続的セッションのためにユーザをデータベースに記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # ユーザの永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 
   def log_out
+    forget(current_user)
     reset_session
     @current_user = nil
   end
 
-  # 現在ログイン中のユーザーを返す (いる場合)
+  #記憶トークンcookieに対応するユーザーを返す
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
+
 
   # ユーザーがログインしている場合 true、そうでない場合 false を返す
   def logged_in?
